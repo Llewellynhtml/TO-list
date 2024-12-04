ad40dca071501277f5a450e6a52cb1ee52f40a90
Llewellyn <Llewellyn.ml.info@gmail.com>
2024-12-04 14:34:17 +0200

    ToDoList

--- db/server.js
+++ db/server.js
@@ -6,7 +6,7 @@ const db = require("better-sqlite3")("database.db");
 const bcrypt = require("bcrypt");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3004;
 
 app.use(cors());
 app.use(bodyParser.json());
